d0690e4b1da71dc163e61d514bcc444e0d1d203e
Kai Striega <kaistriega@gmail.com>
2017-12-13 17:42:50 +0800

    Add sample RSS feed to fixtures and write first test for parse_feed function

--- tests.py
+++ tests.py
@@ -1,5 +1,7 @@
 import unittest
+from collections import namedtuple
 import fuelscanner
+
 
 class TestUrlFormatter(unittest.TestCase):
 
@@ -19,6 +21,43 @@ class TestUrlFormatter(unittest.TestCase):
         )
         actual_urls = fuelscanner.format_url(url, suburbs)
         self.assertEqual(actual_urls, excpected_urls)
+
+
+class TestFeedParser(unittest.TestCase):
+
+    def test_correct_station_are_returned_from_feed_parser(self):
+        Station = namedtuple('Station', 'name address brand price')
+        expected_station_1 = Station(
+            name='Caltex Woolworths Beckenham',
+            address='63 William St, BECKENHAM',
+            brand='Caltex Woolworths',
+            price=128.9
+        )
+        expected_station_2 = Station(
+            name='Caltex StarMart Bassendean',
+            address='309 Guildford Rd (Cnr North Rd), BASSENDEAN',
+            brand='Caltex',
+            price=129.9
+        )
+        expected_station_3 = Station(
+            name='Shell Gidgegannup',
+            address='2095 Toodyay Rd, GIDGEGANNUP',
+            brand='Shell',
+            price=137.9
+        )
+        un_expected_station = Station(
+            name='Vibe Mt Helena',
+            address='9 McVicar Pl',
+            brand='Vibe',
+            price=126.9
+        )
+
+        actual_stations = fuelscanner.parse_feed('fixtures/testfeedone.xml')
+
+        self.assertIn(expected_station_1, actual_stations)
+        self.assertIn(expected_station_2, actual_stations)
+        self.assertIn(expected_station_3, actual_stations)
+        self.assertNotIn(un_expected_station, actual_stations)
 
 
 if __name__ == '__main__':
