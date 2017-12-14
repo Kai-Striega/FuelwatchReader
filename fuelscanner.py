a3ea48a3a4917d8c465fdd51ca7ba7d917170a0b
Kai Striega <kaistriega@gmail.com>
2017-12-14 19:02:18 +0800

    Refactor Station namedtuple into class.

--- fuelscanner.py
+++ fuelscanner.py
@@ -1,12 +1,60 @@
 #!/usr/bin/env python
 
 from itertools import product
+import feedparser
+
+
+class Station(object):
+    """Fuel Station summary object."""
+
+    def __init__(self, name, address, price, discount=0):
+        """Instantiate Station object.
+
+        Args:
+            name (str): Business name of the fuel station.
+            address (str): Address of the station.
+            price (float): Fuel price in cents per litre.
+            discount (int, optional): Discount of fuel in cents per litre.
+        """
+        self.name = name
+        self.address = address
+        self.price = price
+        self.discount = discount
+
+    def __eq__(self, other):
+        """Override default Equals behaviour."""
+        if isinstance(other, self.__class__):
+            return self.__dict__ == other.__dict__
+        return False
+
+    @property
+    def discounted_price(self):
+        """Fuel price with discount included."""
+        return self.price - self.discount
+
 
 def format_url(feed_url, suburbs):
-    """Format URLs for the FuelWatch RSS feed"""
-
+    """Format URLs for the FuelWatch RSS feed."""
     if isinstance(suburbs, str):
         return '&Suburb='.join([feed_url, suburbs]),
 
     urls = tuple('&Suburb='.join(url) for url in product([feed_url], suburbs))
     return urls
+
+
+def parse_feed(url_set):
+    """Parse the RSS feeds and return list of station summaries."""
+    if isinstance(url_set, str):
+        url_set = [url_set]
+
+    station_summary_list = list()
+    for url in url_set:
+        rss_feed = feedparser.parse(url)
+        for entry in rss_feed.entries:
+            station = Station(
+                name=entry.get('trading-name'),
+                address=entry.get('address'),
+                price=float(entry.get('price'))
+            )
+            station_summary_list.append(station)
+    return station_summary_list
--- tests.py
+++ tests.py
@@ -1,7 +1,26 @@
 import unittest
-from collections import namedtuple
-import fuelscanner
-from fuelscanner import Station
+from fuelscanner import Station, format_url, parse_feed
+
+
+class TestStationClass(unittest.TestCase):
+
+    def test_discounted_price_property(self):
+        station_1 = Station('Station 1', 'Address 1', 128.9, discount=4)
+        self.assertEqual(station_1.discounted_price, 124.9)
+        station_2 = Station('Station 2', 'Address 2', 125.0)
+        self.assertEqual(station_2.discounted_price, 125.0)
+        station_2.discount = 4
+        self.assertEqual(station_2.discounted_price, 121.0)
+
+    def test_station_equality(self):
+        """Station attributes must all be equal for Station equality."""
+        station_a = Station('Station 1', 'Address 1', 100)
+        station_b = Station('Station 1', 'Address 1', 100)
+        station_c = Station('Not Station 1', 'Address 1', 100)
+
+        self.assertEqual(station_a, station_b)
+        self.assertNotEqual(station_a, station_c)
+        self.assertNotEqual(station_b, station_c)
 
 
 class TestUrlFormatter(unittest.TestCase):
@@ -10,7 +29,7 @@ class TestUrlFormatter(unittest.TestCase):
         url = 'sample_url'
         suburb = 'SuburbA'
         excpected_url = ('sample_url&Suburb=SuburbA',)
-        actual_url = fuelscanner.format_url(url, suburb)
+        actual_url = format_url(url, suburb)
         self.assertEqual(actual_url, excpected_url)
 
     def test_urls_with_two_suburbs(self):
@@ -20,55 +39,43 @@ class TestUrlFormatter(unittest.TestCase):
             'sample_url&Suburb=SuburbA',
             'sample_url&Suburb=SuburbB'
         )
-        actual_urls = fuelscanner.format_url(url, suburbs)
+        actual_urls = format_url(url, suburbs)
         self.assertEqual(actual_urls, excpected_urls)
 
 
 class TestFeedParser(unittest.TestCase):
 
-    def setUp(self):
-        self.expected_station_1 = Station(
-            name='Caltex Woolworths Beckenham',
-            address='63 William St',
-            brand='Caltex Woolworths',
-            price=128.9
-        )
-        self.expected_station_2 = Station(
-            name='Caltex StarMart Bassendean',
-            address='309 Guildford Rd (Cnr North Rd)',
-            brand='Caltex',
-            price=129.9
-        )
-        self.expected_station_3 = Station(
-            name='Shell Gidgegannup',
-            address='2095 Toodyay Rd',
-            brand='Shell',
-            price=137.9
-        )
-        self.un_expected_station = Station(
-            name='Vibe Mt Helena',
-            address='9 McVicar Pl',
-            brand='Vibe',
-            price=126.9
-        )
-
     def test_correct_station_returned_from_parser(self):
-        actual_stations = fuelscanner.parse_feed('fixtures/testfeedone.xml')
-
+        actual_stations = parse_feed('fixtures/testfeedone.xml')
         self.assertTrue(len(actual_stations) == 2)
-        self.assertIn(self.expected_station_1, actual_stations)
-        self.assertIn(self.expected_station_2, actual_stations)
-        self.assertNotIn(self.un_expected_station, actual_stations)
+
+        expected_stations = (
+            Station('Caltex Beckenham', '63 William St', 128.9),
+            Station('Caltex Bassendean', '309 Guildford Rd', 129.9)
+        )
+
+        for station in expected_stations:
+            self.assertIn(station, actual_stations)
+
+        un_expected_station = Station('Vibe Mt Helena', '9 McVicar Pl', 126.9)
+        self.assertNotIn(un_expected_station, actual_stations)
 
     def test_parses_correctly_given_multiple_feeds(self):
         test_urls = ('fixtures/testfeedone.xml', 'fixtures/testfeedtwo.xml')
-        actual_stations = fuelscanner.parse_feed(test_urls)
-
+        actual_stations = parse_feed(test_urls)
         self.assertTrue(len(actual_stations) == 3)
-        self.assertIn(self.expected_station_1, actual_stations)
-        self.assertIn(self.expected_station_2, actual_stations)
-        self.assertIn(self.expected_station_3, actual_stations)
-        self.assertNotIn(self.un_expected_station, actual_stations)
+
+        expected_stations = (
+            Station('Caltex Beckenham', '63 William St', 128.9),
+            Station('Caltex Bassendean', '309 Guildford Rd', 129.9),
+            Station('Shell Gidgegannup', '2095 Toodyay Rd', 137.9)
+        )
+        for station in expected_stations:
+            self.assertIn(station, actual_stations)
+
+        un_expected_station = Station('Vibe Mt Helena', '9 McVicar Pl', 126.9)
+        self.assertNotIn(un_expected_station, actual_stations)
+
 
 if __name__ == '__main__':
     unittest.main()
